332979fb14d045ef1d937de013dc62b6ff319273
Roland <roland.onofrej@akademiasovy.sk>
2018-03-28 12:04:05 +0200

    second commit, added generated numbers

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -10,13 +10,22 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Random;
+
 public class Controller {
     public Button btnLogin;
     public Label lblError;
     public PasswordField txtPassword;
     public TextField txtLogin;
-
-
+    public Button btnGenerate;
+    public Label lblNum1;
+    public Label lblNum2;
+    public Label lblNum3;
+    public Label lblNum4;
+    public Label lblNum5;
+    private int[] arr;
 
 
     public void clickLogin(ActionEvent actionEvent) throws Exception{
@@ -32,12 +41,51 @@ public class Controller {
             Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
             Stage newStage = new Stage();
             newStage.setTitle("TIPOS admin portal");
-            newStage.setScene(new Scene(root, 450, 300));
+            newStage.setScene(new Scene(root, 600, 300));
             newStage.show();
             // do what you have to do
             stage.close();
         }
     }
 
+    public void generateNumbers(ActionEvent actionEvent) throws Exception{
+        arr = new int[5];
+        Random random=new Random();
+        arr[0]=random.nextInt(35)+1;
+        int i,j=1,k;
+        boolean match;
+        do{
+            arr[j]=random.nextInt(35)+1;
+            match=false;
+            for(k=0;k<j;k++){
+                if(arr[j]==arr[k]){
+                    match=true;
+                }
+            }
+            if(match==false){
+                j++;
+            }
+        }while(j!=5);
+
+        for(i=0;i<4;i++)
+            for(j=0;j<4;j++)
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+
+
+
+                lblNum1.setText(Integer.toString(arr[0]));
+                lblNum2.setText(Integer.toString(arr[1]));
+                lblNum3.setText(Integer.toString(arr[2]));
+                lblNum4.setText(Integer.toString(arr[3]));
+                lblNum5.setText(Integer.toString(arr[4]));
+
+
+
+
+    }
 
 }
